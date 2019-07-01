0c5b35cc502e276c41c9d5e98333836ef28e1c98
Stephen Parker <stephenparkhum@outlook.com>
2019-07-01 16:37:58 -0700

    Updating redirect URI

--- main.js
+++ main.js
@@ -30,8 +30,8 @@ function getHashParams() {
 }
 
 const userAuthorize = (clientId) => {
-    let redirect = `http://localhost:5500/search.html`;
-    // let redirect = `https://stephenparkhum.github.io/genriFy/search.html`;
+    // let redirect = `http://localhost:5500/search.html`;
+    let redirect = `https://stephenparkhum.github.io/genriFy/search.html`;
     let url = `https://accounts.spotify.com/authorize?client_id=${clientId}&redirect_uri=${redirect}&scope=user-read-private%20user-read-email&response_type=token&state=123`;
     $('.spotify-sign-btn').on('click', function () {
         $('.auth-link').attr('href', url);
